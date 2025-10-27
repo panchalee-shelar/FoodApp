ed01e4987d5ce11adc8a29862465475fc5b308e6
panchalee-shelar <=>
2025-10-27 13:33:38 +0530

    Updates

---
+++ Grocery.e031d8aa.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire8640;(0,e.register)("loR3J",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var l=e("ayMG0"),o=()=>(0,l.jsx)("h1",{children:"This is Grocery services! with another bundler file"})});
+//# sourceMappingURL=Grocery.e031d8aa.js.map
